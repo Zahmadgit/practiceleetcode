70d8658e0250cdf20088b2223129e8fbafdaa13d
Zeshan Ahmad <112338853+Zahmadgit@users.noreply.github.com>
2024-11-12 20:41:24 -0500

    Time: 0 ms (100.00%), Space: 16.9 MB (14.37%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -15,4 +15,8 @@ class Solution:
                 nums1[p] = nums2[p2]
                 p2 -= 1
             p -= 1
-        nums1[:p2 + 1] = nums2[:p2 + 1]
+        
+        while p2 >= 0:
+            nums1[p] = nums2[p2]
+            p -= 1
+            p2 -= 1
